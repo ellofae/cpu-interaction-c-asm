606d0eaf781feef8c84f456050b9b4294f7dd411
Sergei Bykovskii <s.bykovskii@3l.ru>
2024-04-30 02:36:46 +0300

    feat: add misc security features reading

--- src/main.c
+++ src/main.c
@@ -7,7 +7,9 @@
 
 static system_flags flags;
 
+static void print_binary(unsigned int value);
 static void get_cpu_vendor(cpu_registers *registers);
+static void read_misc_security_info_bits(cpu_registers *registers);
 
 int main(int argc, char** argv) {
     flags.arch_config.system_arch = ARCH_X86;
@@ -35,8 +37,24 @@ int main(int argc, char** argv) {
     registers.ecx = 0; 
 
     get_cpu_vendor(&registers);
+    
+    registers.eax = 7;
+    registers.ecx = 0;
+
+    read_misc_security_info_bits(&registers);
 
     return 0;
+}
+
+static void print_binary(unsigned int value) {
+    int num_bits;
+    
+    num_bits = sizeof(unsigned long long) * 8;
+    for (int i = num_bits - 1; i >= 0; i--) {
+        int bit = (value >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
 }
 
 static void get_cpu_vendor(cpu_registers *registers) {
@@ -50,4 +68,27 @@ static void get_cpu_vendor(cpu_registers *registers) {
     vendor_str[sizeof(uint32) * 3] = '\0';
 
     log_message(LOG_INFO, "CPU Vendor - %s\n", vendor_str);
+}
+
+static void read_misc_security_info_bits(cpu_registers *registers) {
+    uint32 result;
+    result = cpuid_call(&registers->eax, &registers->ebx, &registers->ecx, &registers->edx);
+
+    if ((registers->ebx >> 2) & 0x1) {
+        log_message(LOG_INFO, "Software Guard Extensions (SGX) feature - supported\n");
+    } else {
+        log_message(LOG_WARNING, "Software Guard Extensions (SGX) feature - unsupported\n");
+    }
+
+    if ((registers->ebx >> 7) & 0x1) {
+        log_message(LOG_INFO, "Supervisor-Mode Execution Prevention (SMEP) feature - supported\n");
+    } else {
+        log_message(LOG_WARNING, "Supervisor-Mode Execution Prevention (SMEP) feature - unsupported\n");
+    }
+
+    if ((registers->ebx >> 20) & 0x1) {
+        log_message(LOG_INFO, "Supervisor-Mode Access Prevention (SMAP) feature - supported\n");
+    } else {
+        log_message(LOG_WARNING, "Supervisor-Mode Access Prevention (SMAP) feature - unsupported\n");
+    }
 }
